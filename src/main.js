079694824ad101723120d77ddfe6316ce444f7a7
源仙笙 <xzy_1226@163.com>
2019-10-26 21:29:15 +0800

    设置拦截器和路由守卫

--- src/main.js
+++ src/main.js
@@ -4,28 +4,57 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Vant from 'vant'
+import {Toast} from 'vant';
 
-//引入组件库
-import Vant from 'vant';
-//注册组件库
-Vue.use(Vant);
+
 //引入组件样式文件
 import 'vant/lib/index.css';
+//注册组件库
+Vue.use(Vant);
+
+// 设置轻提示框的展示时长
+Toast.setDefaultOptions({
+  duration: 1000
+})
 
 //设置全局的基准url
-axios.defaults.baseURL='http://127.0.0.1:3000';
+axios.defaults.baseURL = 'http://127.0.0.1:3000';
 
-import { Toast } from 'vant';
+// 设置请求拦截器
+axios.interceptors.request.use(config => {
+  //设置请求头
+  config.headers.Authorization = localStorage.getItem('token');
+  return config
+})
+
 //设置响应拦截器
-axios.interceptors.response.use((res)=> {
-  const {message,statusCode}=res.data;
-  if(message && statusCode===401){
-    Toast.fail(message);
-  }
+axios.interceptors.response.use((res) => {
+  const {
+    message,
+    statusCode
+  } = res.data;
+  //请求失败提示信息
+  if (message && statusCode === 401 && !router.currentRoute.name.includes('index')) Toast.fail(message);
+
   return res;
 });
 
-Vue.prototype.$axios=axios;
+
+//路由守卫
+router.beforeEach((to, from, next) => {
+
+  if (to.path == '/profile') {
+    localStorage.getItem('token') ? next() : next('/login')
+  }
+
+  next();
+
+})
+
+
+
+Vue.prototype.$axios = axios;
 
 
 
@@ -36,6 +65,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
